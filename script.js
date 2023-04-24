cfb81475911684323e84b336dc772cf33239d7ea
Ildar <email@examle.com>
2023-04-24 21:16:07 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,14 @@
+//скрипт выполняется после загрузки страницы
+$(document).ready(function() {
+
+    $('.main_btna, .main_btn, a[href="#sheldure"]').click(function() {
+        $('.overlay').fadeIn(1000);
+        $('.modal').slideDown(1000);
+    });
+
+    $('.close').click(function() {
+        $('.modal').slideUp(1000);
+        $('.overlay').fadeOut(1000);
+    });
+
+});
